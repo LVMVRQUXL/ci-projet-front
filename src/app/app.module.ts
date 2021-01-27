200890ef0fc8d628d3199ebe7bae00748be6ece2
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-27 15:08:09 +0100

    chore: [#1] :building_construction: Add core, features and shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule]
+  imports: [BrowserModule, CoreModule, SharedModule]
 })
 export class AppModule {}
