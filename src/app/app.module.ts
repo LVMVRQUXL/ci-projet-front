6d656e3236cffae0800d91171a1531fd28068fa4
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-27 15:18:18 +0100

    chore: [#1] :heavy_plus_sign: Add Angular material v10.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,6 +9,6 @@ import { SharedModule } from './shared/shared.module';
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, SharedModule]
+  imports: [BrowserAnimationsModule, BrowserModule, CoreModule, SharedModule]
 })
 export class AppModule {}
