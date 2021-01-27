1889dc8d7ff13359e997c121592e43c54635d346
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-27 15:02:03 +0100

    refactor: [#1] :recycle: Refactor all project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule]
 })
-export class AppModule { }
+export class AppModule {}
