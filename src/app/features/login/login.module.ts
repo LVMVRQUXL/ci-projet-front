c4dc8ba59d0efa0a08895594422d3de65eae0fce
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-28 22:58:42 +0100

    style: [#3] :lipstick: Update login's UI

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '@app/shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginPage } from './pages';
 
 @NgModule({
   declarations: [LoginPage],
-  imports: [CommonModule, LoginRoutingModule]
+  imports: [CommonModule, LoginRoutingModule, SharedModule]
 })
 export class LoginModule {}
