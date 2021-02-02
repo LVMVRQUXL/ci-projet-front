35febcd33204f3405fedf0b59923d4765951e80e
Loïc Lamarque <loiclamarque777@gmail.com>
2021-02-03 00:13:53 +0100

    refactor: [#6] :recycle: Refactor file uploading

--- src/app/core/services/upload.service.ts
+++ src/app/core/services/upload.service.ts
@@ -1,49 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFireUploadTask } from '@angular/fire/storage';
-import firebase from 'firebase/app';
-import { Observable } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
 
 @Injectable()
 export class UploadService {
-  task: AngularFireUploadTask;
+  constructor(private _storage: AngularFireStorage) {}
 
-  percentage: Observable<number>;
-  snapshot: Observable<any>;
-  downloadURL: string;
-
-  private _filesRef = firebase.storage().ref().child('files');
-
-  constructor() {}
-
-  upload(file: File) {
-    const fileName = `${Date.now()}_${file.name}`;
-
-    console.debug(`Uploading ${fileName}...`);
-
-    const uploadTask = this._filesRef.child(fileName).put(file);
-
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on(
-      firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-      (snapshot) => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.debug('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case firebase.storage.TaskState.PAUSED: // or 'paused'
-            console.debug('Upload is paused');
-            break;
-          case firebase.storage.TaskState.RUNNING: // or 'running'
-            console.debug('Upload is running');
-            break;
-        }
-      },
-      () => {
-        // Upload completed successfully, now we can get the download URL
-        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-          console.debug('File available at', downloadURL);
-        });
-      }
-    );
+  upload(file: File, fileName: String, onFinalize: (downloadURL) => void) {
+    const filePath: string = `files/${fileName}`;
+    const task = this._storage.upload(filePath, file);
+    const fileRef = this._storage.ref(filePath);
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() =>
+          fileRef
+            .getDownloadURL()
+            .toPromise()
+            .then((value) => onFinalize(value))
+        )
+      )
+      .subscribe();
   }
 }
--- src/app/features/files/pages/upload/upload.page.ts
+++ src/app/features/files/pages/upload/upload.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { UploadService } from '@app/core/services';
+import { NotificationService, UploadService } from '@app/core/services';
 
 @Component({
   selector: 'app-upload',
@@ -10,21 +10,30 @@ import { UploadService } from '@app/core/services';
 export class UploadPage implements OnInit {
   files: File[] = [];
 
-  constructor(private _uploadService: UploadService) {}
+  constructor(
+    private _notificationService: NotificationService,
+    private _uploadService: UploadService
+  ) {}
 
   ngOnInit(): void {}
 
   onSelect(event) {
-    console.log(event);
+    console.debug(event);
     this.files.push(...event.addedFiles);
   }
 
   onRemove(event) {
-    console.log(event);
+    console.debug(event);
     this.files.splice(this.files.indexOf(event), 1);
   }
 
   upload() {
-    this.files.forEach((file: File): void => this._uploadService.upload(file));
+    this.files.forEach((file: File): void => {
+      const fileName = `${Date.now()}_${file.name}`;
+      this._uploadService.upload(file, fileName, (downloadURL) => {
+        console.debug(`downloadURL = ${downloadURL}`);
+        this._notificationService.notify(`Partage terminé !`);
+      });
+    });
   }
 }
