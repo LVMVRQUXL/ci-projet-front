dc7a48f61bb5469edcb2b39df0a404fc82fd1599
Loïc Lamarque <loiclamarque777@gmail.com>
2021-02-06 10:31:11 +0100

    feat: [#7] :sparkles: Add file downloading in browser

--- src/app/core/services/download.service.ts
+++ src/app/core/services/download.service.ts
@@ -1,14 +1,37 @@
-import {Injectable} from '@angular/core';
-import {AngularFireStorage} from "@angular/fire/storage";
+import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { saveAs } from 'file-saver';
+
+import { NotificationService } from './notification.service';
 
 @Injectable()
 export class DownloadService {
   constructor(
+    private _notificationService: NotificationService,
     private _storage: AngularFireStorage
   ) {}
 
-
-  getUrl(fileUrl: string) {
-    return this._storage.refFromURL(fileUrl).getDownloadURL();
+  // TODO: An error should occurre when given link is invalid
+  downloadFromURL(fileUrl: string) {
+    this._storage
+      .refFromURL(fileUrl)
+      .getDownloadURL()
+      .toPromise()
+      .then((url: string) => {
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = (_) => {
+          var blob = xhr.response;
+          saveAs(blob);
+          this._notificationService.notify(
+            'Le fichier a bien été téléchargé !'
+          );
+        };
+        xhr.open('GET', url);
+        xhr.send();
+      })
+      .catch((_) => {
+        this._notificationService.notify('Ce lien est invalide !');
+      });
   }
 }
--- src/app/features/download/pages/download/download.page.ts
+++ src/app/features/download/pages/download/download.page.ts
@@ -1,39 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {DownloadService, NotificationService} from "@app/core/services";
-import {NgForm} from "@angular/forms";
+import { NgForm } from '@angular/forms';
 
-declare var require: any
-const FileSaver = require('file-saver')
+import { DownloadService } from '@app/core/services';
 
 @Component({
   selector: 'app-download',
   templateUrl: './download.page.html',
   styleUrls: ['./download.page.css']
 })
-
 export class DownloadPage implements OnInit {
+  constructor(private _downloadService: DownloadService) {}
 
-  constructor(
-      private _notificationService: NotificationService,
-      private _downloadService: DownloadService) {};
-
-  ngOnInit(): void {
-  }
-
-  private hasError = false;
-
-  unavailableUrl() { return this.hasError; }
+  ngOnInit(): void {}
 
   download(form: NgForm) {
     let fullUrl = form.value['downloadUrl'];
-    try {
-      this._downloadService.getUrl(fullUrl);
-      FileSaver.saveAs(fullUrl);
-      this.hasError = false;
-    }catch (e) {
-      console.log(e.toString());
-      this.hasError = true;
-    }
+    this._downloadService.downloadFromURL(fullUrl);
   }
-
 }
